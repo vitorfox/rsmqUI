a472ce55773a29dfb1102e4d5b2dd68669ac4c5e
Vitor Teixeira <vitorfox@gmail.comm>
2017-04-28 22:20:19 -0300

    Listing queues from redis and show info

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 var express = require("express");
-var app = express();
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}))
+
+var RedisSMQ = require("rsmq")
+var rsmq = new RedisSMQ({
+  host: process.env.REDIS_HOST     || "redis",
+  port: process.env.REDIS_PORT     || 6379,
+  ns:   process.env.RSQM_NAMESPACE || "rsmq",
+})
 
 /* serves main page */
 app.get("/", function(req, res) {
-  res.send("OK");
-});
+  rsmq.listQueues( function (err, queues) {
+    if(err){
+      console.error(err)
+      res.send(err);
+      return
+    }
 
-app.post("/user/add", function(req, res) {
-/* some server side logic */
-  res.send("OK");
-});
+    if (queues.length == 0) {
+      res.send("No queues.");
+    }
+
+    var attrCount = 0
+    var response = []
+    var getAttrFinish = (item) => {
+      attrCount++
+      response.push(item)
+
+      if (attrCount >= queues.length) {
+        res.send(response)
+      }
+    }
+
+    queues.forEach((item) => {
+      rsmq.getQueueAttributes({qname:item}, function (err, resp) {
+        getAttrFinish({queue: item, data:resp})
+      })
+    })
+  })
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
